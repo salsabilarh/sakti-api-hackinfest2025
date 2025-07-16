c0012a952f4b2e80c17e51ac0a692f1754df82e5
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-07-16 17:48:19 +0700

    commet: test config.js

--- config/config.js
+++ config/config.js
@@ -5,13 +5,13 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'mysql',
   },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'sakti',
-    host: '127.0.0.1',
-    dialect: 'mysql'
-  },
+  // test: {
+  //   username: 'root',
+  //   password: null,
+  //   database: 'sakti',
+  //   host: '127.0.0.1',
+  //   dialect: 'mysql'
+  // },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
