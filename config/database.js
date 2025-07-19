2763be9c094376a030cf9ca0961cc32e7a3f76ef
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-07-20 05:32:48 +0700

    fix: error database.js

--- config/database.js
+++ config/database.js
@@ -3,23 +3,16 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: console.log,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
-);
+});
 
 // Test the connection
 (async () => {
