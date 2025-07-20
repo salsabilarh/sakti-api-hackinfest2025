6608a007bb14668ad8a261d076c9ddb90837677c
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-07-20 22:18:40 +0700

    fix: cors

--- app.js
+++ app.js
@@ -25,18 +25,8 @@ const limiter = rateLimit({
 });
 
 // Middleware
-const allowedOrigins = [
-  'http://localhost:5173'
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: ['http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
