8187e90433a0f30329eb5ef40f7bb82053d5d1ba
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-07-22 10:23:24 +0700

    fix download marketing kit

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -3,18 +3,17 @@ const express = require('express');
 const router = express.Router();
 const serviceController = require('../controllers/serviceController');
 const authMiddleware = require('../middlewares/authMiddleware');
-const upload = require('../middlewares/uploadCloudinary');
 
 // Public routes (viewer access)
 router.get('/', authMiddleware.authenticate, serviceController.getAllServices);
 router.get('/:id', authMiddleware.authenticate, serviceController.getServiceById);
 
 // Protected routes (management and admin access)
-router.post('/', 
-  upload.single('file'), 
+router.post(
+  '/',
   authMiddleware.authenticate,
-  authMiddleware.authorize('admin', 'management'), 
-  serviceController.createMarketingKit
+  authMiddleware.authorize('admin', 'management'),
+  serviceController.createService
 );
 router.put(
   '/:id',
