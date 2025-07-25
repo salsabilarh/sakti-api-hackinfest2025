3bf22f6998019fac866234cb23cd1bc2554b13b6
Salsabila Rafifah <salsabilarafifahh@student.ce.undip.ac.id>
2025-07-25 14:24:43 +0700

    fix datatypes of name services and service controller

--- models/unit.js
+++ models/unit.js
@@ -1,19 +1,31 @@
-'use strict';
-
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('services', 'name', {
-      type: Sequelize.TEXT,
+module.exports = (sequelize, DataTypes) => {
+  const Unit = sequelize.define('Unit', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING(100),
       allowNull: false,
       unique: true,
-    });
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('services', 'name', {
-      type: Sequelize.STRING(255),
+    },
+    type: {
+      type: DataTypes.ENUM('sbu', 'ppk', 'cabang', 'unit', 'divisi', 'lainnya'),
       allowNull: false,
-      unique: true,
-    });
-  },
+    },
+  }, {
+    tableName: 'units',
+    timestamps: true,
+    underscored: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  });
+
+  Unit.associate = (models) => {
+    Unit.hasMany(models.User, { foreignKey: 'unit_kerja_id', as: 'users' });
+    Unit.hasMany(models.Service, { foreignKey: 'sbu_owner_id', as: 'owned_services' });
+  };
+
+  return Unit;
 };
